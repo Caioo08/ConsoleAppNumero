9fb779c279e37ee5fde65a8faa9c4bbaaee61ae7
Usuario <Usuario@CPD02-M02>
2024-03-05 12:07:59 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppNumero/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppNumero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double conta;
+
+            Console.Write("Digite um número: ");
+            double n1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite outro número: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a operação(*,-,/,+): ");
+            string oper = Console.ReadLine();
+
+            if (oper == "*")
+            {
+                conta = n1 * n2;
+                Console.WriteLine("O resultado será: " + conta);
+                Console.ReadKey();
+            }
+
+            else if (oper == "+")
+            {
+                conta = n1 + n2;
+                Console.WriteLine("O resultado será: " + conta);
+                Console.ReadKey();
+            }
+            else if (oper == "-")
+            {
+                conta = n1 - n2;
+                Console.WriteLine("O resultado será: " + conta);
+                Console.ReadKey();
+            }
+            else if(oper == "/")
+            {
+                conta = n1 / n2;
+                Console.WriteLine("O resultado será: " + conta);
+                Console.ReadKey();
+            }
+
+            
+
+        }
+    }
+}
